8ee7cd2b1af8dc85fef43f9f22b34bc78b877c9b
Himanshu Pathak <himanshu2725pathak@gmail.com>
2026-04-18 17:23:31 +0530

    Authentication done

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -5,5 +5,10 @@ export const Env = {
   PORT: getEnv("PORT", "8000"),
   BASE_URL: getEnv("BASE_URL"),
   MONGO_URI: getEnv("MONGO_URI"),
+  BETTER_AUTH_SECRET: getEnv("BETTER_AUTH_SECRET"),
+  BETTER_AUTH_URL: getEnv("BETTER_AUTH_URL"),
+
+  GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID"),
+  GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET"),
   FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "http://localhost:5173"),
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,8 @@ import { Env } from "./config/env.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
-// import { connectDatabase } from "./config/database.config";
-// import { getAuth } from "./lib/auth";
+import { connectDatabase } from "./config/database.config";
+import { getAuth } from "./lib/auth";
 // import routes from "./routes"
 // import webhookRoutes from "./routes/webhook.route";
 
@@ -26,10 +26,10 @@ app.use(
   })
 )
 
-// app.all("/api/auth/*splat", (req, res) => {
-//   const auth = getAuth();
-//   return toNodeHandler(auth)(req, res);
-// });
+app.all("/api/auth/*splat", (req, res) => {
+  const auth = getAuth();
+  return toNodeHandler(auth)(req, res);
+});
 
 // app.use("/api/webhook", webhookRoutes)
 
@@ -62,6 +62,6 @@ app.get("/health", asyncHandler(async (req: Request, res: Response) => {
 app.use(errorHandler)
 
 app.listen(Env.PORT, async () => {
-  // await connectDatabase()
+  await connectDatabase()
   console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
 })
